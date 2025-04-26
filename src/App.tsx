1ec3389c64f65c267ef47e5141cb08c558387c5c
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-04-26 16:39:14 +0500

    feat: add i18next for internationalization and implement language switching in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 // import viteLogo from '/vite.svg'
+import { Helmet } from 'react-helmet-async'
+import { useTranslation } from 'react-i18next'
 
 function App() {
-  return <h1>Wellcome to, Sparkit Boilerplate</h1>;
+  const { t, i18n } = useTranslation()
+
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang)
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Helmet>
+        <title>Sparkit Boilerplate</title>
+      </Helmet>
+      <div>
+        <h1 className="text-3xl font-bold">{t('welcome')}</h1>
+        <p className="mb-2">{t('description')}</p>
+        <button
+          className="py-2 px-3 bg-stone-800 rounded text-white"
+          onClick={() => changeLanguage('fr')}
+        >
+          Switch to French
+        </button>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/i18n/config.ts
@@ -0,0 +1,28 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+import enTranslation from './locales/en/translation.json'
+
+export const resources = {
+  en: { translation: enTranslation },
+} as const
+
+i18n
+  .use(LanguageDetector) // Detect language
+  .use(initReactI18next) // Pass to react-i18next
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: false, // Set true for development to see logs
+    interpolation: {
+      escapeValue: false, // React already does escaping
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  })
+
+export default i18n
